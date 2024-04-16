306d8be258d36a36b47a4647a8887dd7fdfb87c9
AVIK BANERJEE <avik1.banerjee.20@aot.edu.in>
2024-04-16 20:19:25 +0530

    Currency Files

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import InputBox from './InputBox'
+
+// for exporting we have to just give comas and write in the currly braces
+export {InputBox}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    <App/>
+)
